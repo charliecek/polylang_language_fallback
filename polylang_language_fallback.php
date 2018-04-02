4d4d80ed9fb32ab8eaf7ccb2fca1ca559725214c
charliecek <charliecek@gmail.com>
2018-04-02 17:48:26 +0200

    v1.0.0: user agent exceptions
    Added exceptions to the fallback based on user agent.

--- polylang_language_fallback.php
+++ polylang_language_fallback.php
@@ -4,18 +4,33 @@
  * Description: Set Polylang Fallback Language to English
  * Author: charliecek
  * Author URI: http://charliecek.eu/
- * Version: 0.1
+ * Version: 1.0.0
  */
- 
-$strFallbackLangSlug = 'en';
 
 function srd_pll_preferred_language( $strLangSlug ) {
   // file_put_contents( "/home/clients/multi_156612/salsarueda.dance/srd_pll_preferred_language.debug.txt", var_export($strLangSlug, true). PHP_EOL . var_export(PLL()->options, true) );
+  $strFallbackLangSlug = 'en';
+
+  $aUserAgentsNoFallback = array(
+    'facebookexternalhit',
+    'facebot',
+  );
+  if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $strUserAgent = $_SERVER['HTTP_USER_AGENT'];
+    // file_put_contents(__DIR__."/ua.txt", $strUserAgent);
+  } else {
+    $strUserAgent = "";
+  }
+  foreach ($aUserAgentsNoFallback as $strUserAgentNoFallback) {
+    if (false !== stripos($strUserAgent, $strUserAgentNoFallback)) {
+      return $strLangSlug;
+    }
+  }
 
   // False is when preferred browser language didn't match any of our languages OR when detecting browser languages is disabled //
   if (function_exists('PLL') && PLL()->options['browser']) {
     // Language detecting by browser is enabled //
-    return $strLangSlug === false ? 'en' : $strLangSlug;
+    return $strLangSlug === false ? $strFallbackLangSlug : $strLangSlug;
   } else {
     // Language detecting by browser is disabled //
     return $strLangSlug;
